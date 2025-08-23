51dc40b038268ba48021554053853c7126f695d9
HEFIN01 <kavinsiddarth20@gmail.com>
2025-08-23 21:37:15 +0530

    Create User.js

---
+++ backend/models/User.js
@@ -0,0 +1,13 @@
+// backend/models/User.js
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  icpPrincipal: { type: String, unique: true, sparse: true }, // DFINITY Principal ID
+  date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('User', UserSchema);
